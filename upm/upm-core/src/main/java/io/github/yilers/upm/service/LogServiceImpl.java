a24afa597cc31867cc39ef431811cd331d6bb086
zhanghui <695452483@qq.com>
2026-04-09 10:21:41 +0800

    feat 日志添加租户隔离 用不同平台管理员可以看自己租户下数据

--- upm/upm-core/src/main/java/io/github/yilers/upm/service/LogServiceImpl.java
+++ upm/upm-core/src/main/java/io/github/yilers/upm/service/LogServiceImpl.java
@@ -1,11 +1,13 @@
 package io.github.yilers.upm.service;
 
+import cn.dev33.satoken.stp.StpUtil;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import io.github.yilers.upm.entity.Log;
 import io.github.yilers.upm.mapper.LogMapper;
 import io.github.yilers.upm.response.LogInfoResponse;
 import io.github.yilers.web.base.BasePageRequest;
+import io.github.yilers.web.context.RequestContextHolder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -20,6 +22,8 @@ public class LogServiceImpl extends ServiceImpl<LogMapper, Log> implements LogSe
     @Override
     public Page<LogInfoResponse> findByPage(BasePageRequest<Log> request) {
         Page<?> p = new Page<>(request.getCurrent(), request.getSize());
+        Long tenantId = RequestContextHolder.getContext().getTenantId();
+        request.getData().setTenantId(tenantId);
         return logMapper.findByPage(p, request);
     }
 }
